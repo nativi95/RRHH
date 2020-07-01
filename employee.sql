57899e0753e9cdb2e8141931a424ca1fe3fea117
juan carolos ruiz nativi <juanativi95@gmail.com>
2020-07-01 15:20:29 -0600

    31/07/2020 correcion de BD
    se corrige el tipo varchar en los campos de auditoria por date

--- employee.sql
+++ employee.sql
@@ -25,9 +25,9 @@ CREATE TABLE emp_emp_employee (
     emp_gender      	ENUM ('M','F')  NOT NULL,    
     emp_hire_date   	DATE            NOT NULL,
 	A_user_create 	    VARCHAR(10)     NOT NULL,
-    A_date_create       VARCHAR(10)     NOT NULL,
+    A_date_create       DATE     		NOT NULL,
     A_user_change 	    VARCHAR(10)  	NOT NULL,
-    A_date_change       VARCHAR(10)    	NOT NULL,
+    A_date_change       DATE     		NOT NULL,
     PRIMARY KEY (emp_no)
     
 );
@@ -36,9 +36,9 @@ CREATE TABLE emp_dep_department (
     dep_dept_no     	CHAR(4)         NOT NULL,
     dep_dept_name   	VARCHAR(40)     NOT NULL,
 	A_user_create 	    VARCHAR(10)     NOT NULL,
-    A_date_create       VARCHAR(10)     NOT NULL,
+    A_date_create       DATE     		NOT NULL,
     A_user_change 	    VARCHAR(10)  	NOT NULL,
-    A_date_change       VARCHAR(10)    	NOT NULL,
+    A_date_change       DATE     		NOT NULL,
     PRIMARY KEY (dep_dept_no),
     UNIQUE  KEY (dep_dept_name)
 );
@@ -49,9 +49,9 @@ CREATE TABLE emp_dmr_dept_manager (
    dmr_from_date    DATE            NOT NULL,
    dmr_to_date      DATE            NOT NULL,
    A_user_create 	VARCHAR(10)     NOT NULL,
-   A_date_create    VARCHAR(10)     NOT NULL,
+   A_date_create    DATE     		NOT NULL,
    A_user_change 	VARCHAR(10)  	NOT NULL,
-   A_date_change    VARCHAR(10)    	NOT NULL,
+   A_date_change    DATE     		NOT NULL,
    FOREIGN KEY (dmr_emp_no)  REFERENCES emp_emp_employee (emp_no)    ON DELETE CASCADE,
    FOREIGN KEY (dmr_dept_no) REFERENCES emp_dep_department (dep_dept_no) ON DELETE CASCADE,
    PRIMARY KEY (dmr_emp_no, dmr_dept_no)
@@ -63,9 +63,9 @@ CREATE TABLE emp_dem_dept_emp (
     dmr_from_date   	DATE            NOT NULL,
     dmr_to_date     	DATE            NOT NULL,
 	A_user_create 	    VARCHAR(10)     NOT NULL,
-    A_date_create       VARCHAR(10)     NOT NULL,
+    A_date_create       DATE     		NOT NULL,
     A_user_change 	    VARCHAR(10)  	NOT NULL,
-    A_date_change       VARCHAR(10)    	NOT NULL,
+    A_date_change       DATE     		NOT NULL,
     FOREIGN KEY (dmr_emp_no)  REFERENCES emp_emp_employee  (emp_no)  ON DELETE CASCADE,
     FOREIGN KEY (dmr_dept_no) REFERENCES emp_dep_department (dep_dept_no) ON DELETE CASCADE,
     PRIMARY KEY (dmr_emp_no,dmr_dept_no)
@@ -77,9 +77,9 @@ CREATE TABLE emp_tit_title (
     tit_from_date   	DATE            NOT NULL,
     tit_to_date     	DATE,
 	A_user_create 	    VARCHAR(10)     NOT NULL,
-    A_date_create       VARCHAR(10)     NOT NULL,
+    A_date_create       DATE     		NOT NULL,
     A_user_change 	    VARCHAR(10)  	NOT NULL,
-    A_date_change       VARCHAR(10)    	NOT NULL,
+    A_date_change       DATE     		NOT NULL,
     FOREIGN KEY (tit_emp_no) REFERENCES emp_emp_employee (emp_no) ON DELETE CASCADE,
     PRIMARY KEY (tit_emp_no,tit_title, tit_from_date)
 ) 
@@ -91,9 +91,9 @@ CREATE TABLE emp_pay_payroll (
     pay_from_date   	DATE            NOT NULL,
     pay_to_date     	DATE            NOT NULL,
 	A_user_create 	    VARCHAR(10)     NOT NULL,
-    A_date_create       VARCHAR(10)     NOT NULL,
+    A_date_create       DATE     		NOT NULL,
     A_user_change 	    VARCHAR(10)  	NOT NULL,
-    A_date_change       VARCHAR(10)    	NOT NULL,
+    A_date_change       DATE     		NOT NULL,
     FOREIGN KEY (pay_emp_no) REFERENCES emp_emp_employee (emp_no) ON DELETE CASCADE,
     PRIMARY KEY (pay_payroll_no,pay_emp_no, pay_from_date)
 ) 
@@ -104,9 +104,9 @@ CREATE TABLE emp_bil_bill (
     bil_value 		   FLOAT(7,2)		NOT NULL,
     bil_description	   VARCHAR(50) 		NOT NULL,
 	A_user_create 	   VARCHAR(10)      NOT NULL,
-    A_date_create      VARCHAR(10)      NOT NULL,
+    A_date_create      DATE     		NOT NULL,
     A_user_change 	   VARCHAR(10)  	NOT NULL,
-    A_date_change      VARCHAR(10)    	NOT NULL,
+    A_date_change      DATE     		NOT NULL,
     FOREIGN KEY (bil_payroll_no) REFERENCES emp_pay_payroll (pay_payroll_no) ON DELETE CASCADE
 
 ) 
@@ -117,9 +117,9 @@ CREATE TABLE emp_rol_rol (
     rol_rol_no         INT             	NOT NULL,
     rol_rol	   		   VARCHAR(10) 		NOT NULL,
 	A_user_create 	   VARCHAR(10)      NOT NULL,
-    A_date_create      VARCHAR(10)      NOT NULL,
+    A_date_create      DATE     		NOT NULL,
     A_user_change 	   VARCHAR(10)  	NOT NULL,
-    A_date_change      VARCHAR(10)    	NOT NULL,
+    A_date_change      DATE     		NOT NULL,
     PRIMARY KEY(rol_rol_no)
 ) ;
 
@@ -129,9 +129,9 @@ CREATE TABLE emp_usr_user (
     user_password	   VARCHAR(130) 	NOT NULL,
     usr_rol_no 		   INT 				NOT NULL,
 	A_user_create 	   VARCHAR(10)      NOT NULL,
-    A_date_create      VARCHAR(10)      NOT NULL,
+    A_date_create      DATE     		NOT NULL,
     A_user_change 	   VARCHAR(10)  	NOT NULL,
-    A_date_change      VARCHAR(10)    	NOT NULL,
+    A_date_change      DATE     		NOT NULL,
     PRIMARY KEY(usr_user_no),
     FOREIGN KEY (usr_rol_no) REFERENCES emp_rol_rol (rol_rol_no) ON DELETE CASCADE
 ) 
