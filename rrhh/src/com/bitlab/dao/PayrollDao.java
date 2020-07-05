db46f525b6a946837fb02626a598c95c1b93ee51
AndRivera <andru2113@gmail.com>
2020-07-05 15:04:57 -0600

    05/07/2020

--- rrhh/src/com/bitlab/dao/PayrollDao.java
+++ rrhh/src/com/bitlab/dao/PayrollDao.java
@@ -13,6 +13,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -91,4 +93,6 @@ public class PayrollDao extends AbstractDao<Payroll>{
         closeJDBCObjects(con, ps, rs); //Cierra la conexión
         return idPayroll;
     }
+    
+    
 }
