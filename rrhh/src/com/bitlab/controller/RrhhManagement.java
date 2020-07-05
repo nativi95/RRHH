a91d21bfcf53c5d5d3d0bf3a423ab8fc3a02e018
AndRivera <andru2113@gmail.com>
2020-07-04 19:22:47 -0600

    04/07/2020 Creacion de menus para gestion
    Menus para la gestion de empleados por parte de rol RRHH

--- rrhh/src/com/bitlab/controller/RrhhManagement.java
+++ rrhh/src/com/bitlab/controller/RrhhManagement.java
@@ -4,82 +4,148 @@
  * and open the template in the editor.
  */
 package com.bitlab.controller;
+import com.bitlab.dao.EmployeeDao;
+import com.bitlab.entities.Employee;
 import java.util.Scanner;
 
 
 import java.sql.SQLException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author Andrea
  */
 public class RrhhManagement {
+    // Variable logger
+    private static Logger logger = LoggerFactory.getLogger(DepartmentManagement.class);
     
+    //Objeto de clase escanner para leer datos
+    Scanner scan = new Scanner(System.in);
+    
+    //String capture donde se guardara los valores de entrada
+    private String capture = " ";
+    
+    //Objeto de clase EmployeeDao
+    private EmployeeDao eDao = new EmployeeDao();
+    
+ /**
+ * Metodo getCapture para realizad la captura de datos
+ * @param string de usuario
+ * @return un objeto de tipo <T>
+ */
     public String getCapture(String user) throws SQLException {
         capture = scan.nextLine();
         
        if(capture.toLowerCase().equals("cancel")){
-           AbstractManagement(user);
+           rrhhManagement(user);
        return null;    
        }else{
        return capture;
        } 
     }
     
-    public void rrhhManagement(String user) throws SQLException {
-
-        //capturo datos
-        //
-        boolean flag = true;
-        while (flag) {
-            System.out.println("Eliga la opción deseada escribiendo la letra correspondiente después presione [enter]:");
-            System.out.println("A. Agregar un nuevo registro");
-            System.out.println("B.Buscar primeros 50 registros");
-            System.out.println("C.Buscar por código identificador registros");
-            System.out.println("D.Buscar similares");
-            System.out.println("E. Regresar al menu anterior");
-           
-            switch (getCapture(user).toLowerCase()) {
-                case "a":
-                    addRecord(user);
-                    flag = true;
-                    break;
-
-                case "b": {
-                    try {
-                        show(user);
-                    } catch (ClassNotFoundException ex) {
-                        Logger.getLogger(UserManagement.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
-                flag = true;
-                break;
-
-                case "c": {
-                    try {
-                        findById(user);
-                    } catch (SQLException ex) {
-                        Logger.getLogger(UserManagement.class.getName()).log(Level.SEVERE, null, ex);
-                    } catch (ClassNotFoundException ex) {
-                        Logger.getLogger(UserManagement.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
-                flag = true;
-                break;
-
-                case "d":
-                    flag = true;
-                    break;
-                case "e":
-                    findLike(user);
-                    flag = false;
-                    break;
-                default:
-                    System.out.println("Escriba una opción valida");
-                    flag = true;
-                    break;
-            }
-
-        }
+    public void addRecord(String user) {
+//        System.out.println("\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+//        logger.debug("Creacion de registro de empleado");
+//        Employee e = new Employee();
+//        
+//        this.getCapture(user);
+//        try {
+//            dDao.create(d);
+//        } catch (SQLException ex) {
+//            logger.error("Ha ocurrido una excepcion en la creacion", ex);
+//        } catch (ClassNotFoundException ex) {
+//            logger.error("Ha ocurrido una excepcion en la creacion", ex);
+//        }
+//        System.out.println("\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
     }
+    
+    public void rrhhManagement(String user) {
+        System.out.println("\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+        boolean search = true;
+
+        System.out.println("Desea ingresar más parámetros de búsqueda escriba la opión y pulse [enter] ");
+        System.out.println("A. Gestion de empleados");        
+        System.out.println("B. Historial de planillas del mes actual");
+        System.out.println("C. Ver Historial de planillas segun mes espacífico");
+        System.out.println("D. Salir");
+        while (search) {
+            try {
+                switch (getCapture(user).toLowerCase()) {
+                    case "a":
+                        show(user);
+                        search = false;
+                        break;
+                    case "b":
+                        findById(user);
+                        search = false;
+                        break;
+                    case "c":
+                        findLike(user);
+                        search = false;
+                        break;
+                    case "d":
+                        menuResultados(user);
+                        search = false;
+                        break;
+                    default:
+                        System.out.println("Escriba una opción valida");
+                        search = true;
+                        break;
+                }
+            } catch (SQLException ex) {
+                logger.error("Ha ocurrido una excepcion en la creacion", ex);
+            } catch (ClassNotFoundException ex) {
+                logger.error("Ha ocurrido una excepcion en la creacion", ex);
+            }
+        }       
+    }
+    
+    public void menuManagement(String user){
+        System.out.println("\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+        boolean search = true;
+
+        System.out.println("Desea ingresar más parámetros de búsqueda escriba la opión y pulse [enter] ");
+        System.out.println("A. Contratación de empleados");
+        System.out.println("B. Actualización de registros empleados");
+        System.out.println("C. Desactivación de empleados por despido");
+        System.out.println("D. Visualización de pagos generados");
+        System.out.println("E. Generación de pagos en planilla");
+        System.out.println("F. Salir");
+        
+        while (search) {
+            try {
+                switch (getCapture(user).toLowerCase()) {
+                    case "a":
+                        show(user);
+                        search = false;
+                        break;
+                    case "b":
+                        findById(user);
+                        search = false;
+                        break;
+                    case "c":
+                        findLike(user);
+                        search = false;
+                        break;
+                    case "d":
+                        menuResultados(user);
+                        search = false;
+                        break;
+                    default:
+                        System.out.println("Escriba una opción valida");
+                        search = true;
+                        break;
+                }
+            } catch (SQLException ex) {
+                logger.error("Ha ocurrido una excepcion en la creacion", ex);
+            } catch (ClassNotFoundException ex) {
+                logger.error("Ha ocurrido una excepcion en la creacion", ex);
+            }
+        }  
+    }
+    
+    
 }
